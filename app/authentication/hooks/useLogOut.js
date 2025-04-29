16a96dec33261c7e6b1f8ab68b457440667a7494
abdillah-9 <sabdillah855@gmail.com>
2025-04-29 07:45:50 +0300

    frank fixing logOut push

--- app/authentication/hooks/useLogOut.js
+++ app/authentication/hooks/useLogOut.js
@@ -1,7 +1,7 @@
 "use client";
 import { useMutation, useQueryClient } from "@node_modules/@tanstack/react-query";
 import { useRouter } from "@node_modules/next/navigation";
-import toast from "@node_modules/react-hot-toast/dist";
+import toast from "@node_modules/react-hot-toast";
 import { Logout as logOutApi } from "@utils/apiAuth";
 
 export default function useLogOut() {
@@ -11,10 +11,13 @@ export default function useLogOut() {
     const {mutate: logOut, isLoading:logOutIsLoading} = useMutation({
         mutationFn:logOutApi,
         onSuccess:()=>{
-            queryClient.removeQueries();
+            queryClient.clear();
             toast.success("You've successfully logged out ");
-            routing.push('/authentication/login',{replace:"true"});
+            routing.push('/authentication/signIn',{replace:"true"});
         },
+        onError:(err)=>{
+            toast.error(err.message)
+        }
     });
     
     return {logOut, logOutIsLoading}
