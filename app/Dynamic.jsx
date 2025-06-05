93ce009d50cd1a06e790b089877e556a3931ee95
abdillah-9 <sabdillah855@gmail.com>
2025-06-05 14:07:34 +0300

    v1.1

--- app/Dynamic.jsx
+++ app/Dynamic.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 
 export default function DynamicPage({children}) {
 
-//Css
-const dynamicPage={
-  width:"100%",
-  height:"90vh",
-  padding:"15px 10px",
-}
+  //Css
+  const dynamicPage={
+    width:"100%",
+    height:"90vh",
+    padding:"15px 10px",
+  }
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://frank-business-ai.onrender.com/wakeUP', {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+
+      } catch (error) {
+        console.error('Connection Err ', error);
+      }
+    };
+
+    fetchData();    
+  }
+  ,[])
 
   return (
     <div style={dynamicPage}>
