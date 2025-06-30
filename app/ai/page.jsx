bf544c1fe7aceb0d7b348e59d5edb5a77fbf665a
abdillah-9 <sabdillah855@gmail.com>
2025-06-30 09:38:36 +0300

    v1

--- app/ai/page.jsx
+++ app/ai/page.jsx
@@ -89,7 +89,7 @@ export default function page() {
             try {
               //'http://localhost:8000/predict'
               //https://frank-business-ai.onrender.com/predict
-              const response = await fetch('http://localhost:8000/predict', {
+              const response = await fetch('https://frank-business-ai.onrender.com/predict', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(budgetStats)
